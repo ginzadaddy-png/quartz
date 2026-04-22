adae2fcb411974c33da77a7ec8b98a364d03b91d
ginzadaddy-png <ginzadaddy@gmail.com>
2026-04-22 16:24:38 +0900

    글자 수정

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,15 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      mapFn: (node) => {
+        // 폴더명 첫글자 대문자로 변환
+        if (node.file === null) {
+          node.displayName =
+            node.displayName.charAt(0).toUpperCase() + node.displayName.slice(1)
+        }
+      },
+    }),
   ],
   right: [
     Component.Graph(),
@@ -62,7 +70,15 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      mapFn: (node) => {
+        // 폴더명 첫글자 대문자로 변환
+        if (node.file === null) {
+          node.displayName =
+            node.displayName.charAt(0).toUpperCase() + node.displayName.slice(1)
+        }
+      },
+    }),
   ],
   right: [],
 }
