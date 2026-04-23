7b35e35cf535142daffe968e9767d08058b71d10
ginzadaddy-png <ginzadaddy@gmail.com>
2026-04-23 15:58:41 +0900

    Update Head.tsx

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -14,7 +14,9 @@ export default (() => {
   }: QuartzComponentProps) => {
     const titleSuffix = cfg.pageTitleSuffix ?? ""
     const title =
-      (fileData.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title) + titleSuffix
+      (fileData.slug === "index"
+        ? cfg.pageTitle
+        : (fileData.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title)) + titleSuffix
     const description =
       fileData.frontmatter?.socialDescription ??
       fileData.frontmatter?.description ??
