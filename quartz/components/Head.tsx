3664395c49e38e7256289ed8515e2e67f91f82ad
ginzadaddy-png <ginzadaddy@gmail.com>
2026-04-24 15:47:50 +0900

    폰트 교체 빌드 실패 수정

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -52,6 +52,8 @@ export default (() => {
             )}
           </>
         )}
+        <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css" />
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
